17779c926a4132bfc2715c75ebfabd8ddbb256e3
Xine <cjdoza@gmail.com>
2021-04-07 15:15:11 -0500

    initial

---
+++ CoffeeShop/Models/BeanVariety.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class BeanVariety
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength=3)]
+        public string Name { get; set; }
+        [Required]
+
+
+    }
+}

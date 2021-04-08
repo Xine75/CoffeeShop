879408048c2fc789a356653778617ac954ae7a75
Xine <cjdoza@gmail.com>
2021-04-08 11:46:09 -0500

    full crud coffee

--- js-app/main.js
+++ js-app/main.js
@@ -1,4 +1,5 @@
 const url = "https://localhost:5001/api/beanvariety/";
+const url2 = "https://localhost:5001/api/coffee/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
@@ -6,8 +7,16 @@ button.addEventListener("click", () => {
         .then(beanVarieties => {
             console.log(beanVarieties);
         })
+    getAllCoffees()
+        .then(coffees => {
+            console.log(coffees);
+        })
 });
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+function getAllCoffees() {
+    return fetch(url2).then(resp => resp.json());
 }
